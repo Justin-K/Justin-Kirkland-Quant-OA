44962c09933eebc6b20ec073b0a5d06b09df4f83
Justin K <justinj.kirkland@gmail.com>
2022-09-18 13:47:40 -0500

    New functionality in scraper.py; now, we'll scrape submission posted after a "cut-off" date.

--- reddit-subreddit-scraper/scraper.py
+++ reddit-subreddit-scraper/scraper.py
@@ -1,14 +1,15 @@
 import datetime
-
+from typing import List
 from praw import Reddit
-from praw.models import Subreddit
+from praw.models import Subreddit, Submission
 from prawcore.exceptions import PrawcoreException
 from errors import SubredditInaccessibleError
 from enums import Timeframe
-from data_classes import SubredditResult
-#from datetime import datetime
+from data_classes import SubredditResult, Post
+
 
 class SubredditScraper:
+    MAX_CACHE: int = 50
 
     def __init__(self, site: str):
         self.client = Reddit(
@@ -28,14 +29,34 @@ class SubredditScraper:
             raise SubredditInaccessibleError(f"The subreddit \"r/{subreddit_name}\" is inaccessible.") from e
         return sreddit
 
+    def __process_cache(self, cache: List[Submission]):
+        pass
+
     def scrape_subreddit(self,
                          subreddit_name: str,
-                         timeframe: Timeframe = Timeframe.WEEK,
-                         limit: int = 5):
+                         timeframe: Timeframe = Timeframe.ALL,
+                         timeframe_scalar: int = 1,
+                         ):
         subreddit = self.validate_subreddit(subreddit_name)
-        result: SubredditResult = SubredditResult()
-        for post in subreddit.top(time_filter=timeframe.value, limit=10):
-            pass
-            # print(datetime.datetime.utcfromtimestamp(post.created_utc), post.id)
-            # for comment in post.comments:
-            #     pass
+        if timeframe == Timeframe.YEAR and timeframe_scalar > 1:
+            timeframe = Timeframe.ALL
+        if timeframe is not Timeframe.ALL:
+            start: int = int((datetime.datetime.utcnow() - datetime.timedelta(
+                milliseconds=timeframe.value * timeframe_scalar
+                )).timestamp())
+        else:
+            start: int = int(datetime.datetime(1970, 1, 1).timestamp())
+        cache: List[Submission] = []
+
+        for post in subreddit.top(time_filter=timeframe.name.lower()):
+            if post.created_utc > start:
+                if len(cache) >= SubredditScraper.MAX_CACHE:
+                    self.__process_cache(cache)
+                    cache.clear()
+                else:
+                    cache.append(post)
+        if cache:
+            self.__process_cache(cache)
+            cache.clear()
+
+
