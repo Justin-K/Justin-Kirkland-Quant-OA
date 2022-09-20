2cc3f81f342623abd5b0a8bb4fc28faa2a5cc714
Justin K <justinj.kirkland@gmail.com>
2022-09-20 16:50:26 -0500

    Major changes 1. Deleted all data classes because I need to rewrite them 2. Introduced get_top_submissions_and_comments() (its been tested and IT WORKS) 3. Updated TODO.md

--- reddit-subreddit-scraper/TODO.md
+++ reddit-subreddit-scraper/TODO.md
@@ -2,8 +2,9 @@
 # 1. [ ] Add a filter attribute to pmaw search after I know what fields I what to collect.
 # 2. [ ] Make the SubredditScraper class more flexible. Instead of only getting the top 5 posts, maybe a
 # function the user can pass?
-# 3. [ ] Investigate passing **kwargs to function(s)
-# 4. [ ] Add documentation to README file and to source code
+# 3. [ ] Add documentation to README file and to source code
+# 4. [ ] Dataclass to represent a time period? (a start date(time), and an end date(time))
+# 5. [ ] Add check to scrape_subreddit() method to ensure that the before date isn't after the end date
 
 # Finished
 # 1. [x] Optimize the scrape_subreddit() method. The method should probably contact the API less. 
--- reddit-subreddit-scraper/scraper.py
+++ reddit-subreddit-scraper/scraper.py
@@ -1,19 +1,26 @@
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 from typing import List
 from praw import Reddit
 from praw.models import Subreddit, Submission
+from praw.models.comment_forest import CommentForest
+from pmaw import Response
 from prawcore.exceptions import PrawcoreException
 from errors import SubredditInaccessibleError
 from pmaw import PushshiftAPI
+from concurrent.futures import ThreadPoolExecutor
 
 
 class SubredditScraper:
+
+    # all api calls return lazy objects; the actual data is only fetched from the api when it is "needed"
 
     def __init__(self, site: str):
         self.client = Reddit(
             site,
             user_agent="script:quant_oa_bot:v1 (by u/Anon_4306)"
         )
+        self.pmaw_client = PushshiftAPI(praw=self.client, num_workers=15, jitter="decorr")
 
     def validate_subreddit(self, subreddit_name: str) -> Subreddit:
         if subreddit_name[0: 2] == "r/" or subreddit_name[0: 2] == "R/":
@@ -31,21 +38,40 @@ class SubredditScraper:
                          subreddit_name: str,
                          start_date: datetime,
                          end_date: datetime,
-                         filter_fn=lambda t: t["score"] > 0) -> List[Submission]:
+                         filter_fn=lambda t: t["score"] > 0, **kwargs) -> Response:
         subreddit = self.validate_subreddit(subreddit_name)
-        pmaw_client = PushshiftAPI(praw=self.client)
-        search = pmaw_client.search_submissions(after=int(start_date.timestamp()),
-                                                before=int(end_date.timestamp()),
-                                                limit=None,
-                                                subreddit=subreddit,
-                                                filter_fn=filter_fn
-                                                )
+
+        search = self.pmaw_client.search_submissions(after=int(start_date.timestamp()),
+                                                     before=int(end_date.timestamp()),
+                                                     subreddit=subreddit,
+                                                     filter_fn=filter_fn,
+                                                     )
         return search
 
     def get_top_submissions(self, subreddit: str,
                             start_date: datetime,
                             end_date: datetime,
-                            limit=5) -> List[Submission]:
-        search = self.scrape_subreddit(subreddit, start_date, end_date)
-        s_search = sorted([post for post in search], key=lambda x: x["score"], reverse=True)
+                            limit=5) -> List[Response]:
+        search = self.scrape_subreddit(subreddit,
+                                       start_date,
+                                       end_date,
+                                       )
+
+        search = [i for i in search]
+        s_search = sorted(search, key=lambda x: x["score"], reverse=True)
         return s_search[:limit]
+
+    def get_top_submissions_and_comments(self, subreddit, start_date, end_date):
+        search = self.get_top_submissions(subreddit,
+                                          start_date,
+                                          end_date)
+        submission_ids = [i["id"] for i in search]
+        submissions = [self.client.submission(i) for i in submission_ids]
+
+        def sort_(submission):
+            submission.comment_sort = "top"
+            submission.comments = submission.comments.list()
+
+        with ThreadPoolExecutor(max_workers=13) as exe:
+            exe.map(sort_, submissions)
+        return submissions
